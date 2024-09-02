f077d9167c985e3ff8623bb71477a875b68acbf8
Catalina <116193295+Catalina236@users.noreply.github.com>
2024-09-02 15:46:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,58 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="estilos/style.css">
+    <script src="js/code.jquery.com_jquery-3.7.1.min.js"></script>
+    <link rel="icon" href="imagenes/logosena.png">
+    <title>BANIN</title>
+</head>
+<body>
+    <header>
+    <a href="index.php"><img class="logo" src="imagenes/logo-sena-blanco.png" alt=""></a> 
+       <nav class="menu">
+       <ul class="menu-principal">
+            <li><a href="">Candidatos</a></li>
+            <li><a href="BANIN/Evaluar/Evaluacion.php">Proceso de Evaluación</a></li>
+            <li><a href="">Criterios</a></li>
+            <li><a href="">Seleccionados</a></li>
+        </ul>
+       </nav>
+       <a href="BANIN/iniciarsesion.php"><button class="boton_ir">Iniciar sesión</button></a>
+    </header>
+    <div class="contenedor">
+        <div id="banner">
+        <div id="slider" class="slider">
+        <div id="caja" class="caja">
+        <section class="section_caja">
+        <div class="texto">
+            <h1>Sistema de Evaluación de Instructores</h1>
+            <p>Herramienta integral para evaluar y potenciar habilidades de nuestros instructores</p>
+            </div>
+            <section class="section_caja">
+                <img src="imagenes/Imagen.jpg" alt="" class="img_caja">
+            </section>
+            <img src="imagenes/imagenfondo.jpg" class="img_caja" alt="">
+            </section>
+            <section class="section_caja">
+                <img src="imagenes/instructorSENA.jpg" class="img_caja" alt="">
+            </section>
+        </div>
+        </div>
+        <!---<label for="r3" class="arrow left">&#10094;</label>
+        <label for="r2" class="arrow right">&#10095;</label>-->
+        <div id="btn_atras" class="btn_atras">&#60;</div>
+        <div id="btn_siguiente" class="btn_siguiente">&#62;</div>
+        <script src="JS/slider.js"></script>
+    </div>
+<footer>
+</footer>
+</body>
+</html>
+<!---<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/style.css">
     <link rel="icon" href="imagenes/logosena.png">
     <title>BANIN</title>
 </head>
@@ -34,7 +86,7 @@
                     </div>
                 </div>
                 <div class="slide s2">
-                    <img src="imagenes/iStock-619069656-e1505862162136.jpg" alt="">
+                    <img src="imagenes/imagenfondo.jpg" alt="">
                 </div>
                 <div class="slide s3">
                     <img src="imagenes/instructorSENA.jpg" alt="">
